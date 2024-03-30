172cab4dd47f78feb3b3beac41c3afd210990c27
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-03-30 15:19:56 +0700

    Revert "eslint"
    This reverts commit b60dccbe5be92839b276862d93f8bfebe38ef391.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,24 +14,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'import/order': [
-      'error',
-      {
-        'groups': ['builtin', 'external', 'internal'],
-        'pathGroups': [
-          {
-            'pattern': 'react',
-            'group': 'external',
-            'position': 'before'
-          }
-        ],
-        'pathGroupsExcludedImportTypes': ['react'],
-        'newlines-between': 'always',
-        'alphabetize': {
-          'order': 'asc',
-          'caseInsensitive': true
-        }
-      }
-    ]
   },
 }
--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  semi: true,
-  tabWidth: 2,
-}
