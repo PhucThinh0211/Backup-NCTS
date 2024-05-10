fb9d94567bfeb58623fcb047f7286ccfc5b86f9a
anhduc2708 <anhduc270801@gmail.com>
2024-05-10 17:44:38 +0700

    finish carousel of news

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,6 @@ import Cards from '@/components/CarouselCard/Cards';
 import '@/pages/Home/HomeStyle.css'
 import { Carousel } from 'antd';
 import { useRef } from 'react';
-// import { Carousel } from 'antd';
-
 
 const newsCards = [
   {
@@ -15,39 +13,32 @@ const newsCards = [
     desc: "Kể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk",
   },
   {
-    img: "",
+  img: "https://s3-alpha-sig.figma.com/img/8a71/6a99/01ebca0161db844308dce7fd1c78ffa8?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JZNjq8DF-xpFFzB7epoLXKkEJQ78cHnWVDX3GuhSe6CZ5IdMTnhLhpFDe4dSqBzavHKcDNIJ5fMRaYZWmD9WEmz447vJfIgAmrM5MFGcXcPic-Y8jx8hmrbdjHPUbuaHl-ChJVjOVfZIeZsaFNpN81CsjQYxV~DXFODXP--Zr5oqJPyeSHHJ-n-8H8ru2XZSnjatZhXjON8ZbyZ5amPAs0-K~SXr53GVisqWDSsZKoWOxYg7Ahbndr9wfbiCZn299SresH~im9qqxEmatsN-INfLFBVqRps0SB956fHKokepg5ez~ICg8LefjR-GmUxX3sHdbYg6BNkDyBREc8Kz9w__",
+  date: "Jun 12, 2023",
+  author: "Hải Minh",
+  title: "Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo",
+  desc: "Kể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk",
+  },
+  {
+    img: "https://s3-alpha-sig.figma.com/img/8a71/6a99/01ebca0161db844308dce7fd1c78ffa8?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JZNjq8DF-xpFFzB7epoLXKkEJQ78cHnWVDX3GuhSe6CZ5IdMTnhLhpFDe4dSqBzavHKcDNIJ5fMRaYZWmD9WEmz447vJfIgAmrM5MFGcXcPic-Y8jx8hmrbdjHPUbuaHl-ChJVjOVfZIeZsaFNpN81CsjQYxV~DXFODXP--Zr5oqJPyeSHHJ-n-8H8ru2XZSnjatZhXjON8ZbyZ5amPAs0-K~SXr53GVisqWDSsZKoWOxYg7Ahbndr9wfbiCZn299SresH~im9qqxEmatsN-INfLFBVqRps0SB956fHKokepg5ez~ICg8LefjR-GmUxX3sHdbYg6BNkDyBREc8Kz9w__",
     date: "Jun 12, 2023",
     author: "Hải Minh",
-    title: "Giải pháp đóng gói vận chuyển đồ",
-    desc: "Kho bãi",  
+    title: "Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo",
+    desc: "Kể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk",
   },
   {
-    img: "",
-    date: "",
-    author: "",
-    title: "Giải pháp đóng gói vận chuyển đồ",
-    desc: "Kho bãi",  
+    img: "https://s3-alpha-sig.figma.com/img/8a71/6a99/01ebca0161db844308dce7fd1c78ffa8?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JZNjq8DF-xpFFzB7epoLXKkEJQ78cHnWVDX3GuhSe6CZ5IdMTnhLhpFDe4dSqBzavHKcDNIJ5fMRaYZWmD9WEmz447vJfIgAmrM5MFGcXcPic-Y8jx8hmrbdjHPUbuaHl-ChJVjOVfZIeZsaFNpN81CsjQYxV~DXFODXP--Zr5oqJPyeSHHJ-n-8H8ru2XZSnjatZhXjON8ZbyZ5amPAs0-K~SXr53GVisqWDSsZKoWOxYg7Ahbndr9wfbiCZn299SresH~im9qqxEmatsN-INfLFBVqRps0SB956fHKokepg5ez~ICg8LefjR-GmUxX3sHdbYg6BNkDyBREc8Kz9w__",
+    date: "Jun 12, 2023",
+    author: "Hải Minh",
+    title: "Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo",
+    desc: "Kể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk",
   },
   {
-    img: "",
-    date: "",
-    author: "",
-    title: "Giải pháp đóng gói vận chuyển đồ",
-    desc: "Kho bãi",  
-  },
-  {
-    img: "",
-    date: "",
-    author: "",
-    title: "Giải pháp đóng gói vận chuyển đồ",
-    desc: "Kho bãi",  
-  },
-  {
-    img: "",
-    date: "",
-    author: "",
-    title: "Giải pháp đóng gói vận chuyển đồ",
-    desc: "Kho bãi",  
+    img: "https://s3-alpha-sig.figma.com/img/8a71/6a99/01ebca0161db844308dce7fd1c78ffa8?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JZNjq8DF-xpFFzB7epoLXKkEJQ78cHnWVDX3GuhSe6CZ5IdMTnhLhpFDe4dSqBzavHKcDNIJ5fMRaYZWmD9WEmz447vJfIgAmrM5MFGcXcPic-Y8jx8hmrbdjHPUbuaHl-ChJVjOVfZIeZsaFNpN81CsjQYxV~DXFODXP--Zr5oqJPyeSHHJ-n-8H8ru2XZSnjatZhXjON8ZbyZ5amPAs0-K~SXr53GVisqWDSsZKoWOxYg7Ahbndr9wfbiCZn299SresH~im9qqxEmatsN-INfLFBVqRps0SB956fHKokepg5ez~ICg8LefjR-GmUxX3sHdbYg6BNkDyBREc8Kz9w__",
+    date: "Jun 12, 2023",
+    author: "Hải Minh",
+    title: "Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo Giải pháp đóng gói vận chuyển đồ của chúng tôi là hoàn hảo",
+    desc: "Kể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk ể từ khi thành lập, NCTS luôn từng bước khẳng định vị thế và uy tín của mình trong lĩnh vực phục vụ hàng hoá hàng Kể từ khi thành lập NCTSádjkabnsdkjasndkjasndkjasndkjasndk",
   },
 ];
 
@@ -61,6 +52,26 @@ export const Home = () => {
   const prev = () => {
     carouselRef.current.prev();
   };
+  const responsiveSettings = [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 9999, // A very large number to ensure the last settings are always applied
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ];
 
   return (
     < div className='bg-white'>
@@ -89,15 +100,14 @@ export const Home = () => {
           autoplay
           infinite={true}
           autoplaySpeed ={5000}
+          responsive={responsiveSettings}
         >
           {newsCards.map((i, index) => (
-            <div key={index}>
-              <Cards img={i.img} date={i.date} author={i.author} title={i.title} desc={i.desc} />
-            </div>
+              <Cards key={index} img={i.img} date={i.date} author={i.author} title={i.title} desc={i.desc} />
           ))}
          
         </Carousel>
-         <div style={{ textAlign: "center"}} className="arrow-btn">
+         <div style={{ textAlign: "center"}} className="arrow-btn d-none d-xl-flex">
             <button className="lr-carousel-button" onClick={prev}>
               <i className="fa-solid fa-arrow-left"></i>
             </button>
