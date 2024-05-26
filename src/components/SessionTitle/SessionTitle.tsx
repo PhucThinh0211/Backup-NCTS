e606eb08cd006d697cd2ff1a730a49e698170137
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-05-26 17:08:20 +0700

    section title

--- src/components/SessionTitle/SessionTitle.tsx
+++ src/components/SessionTitle/SessionTitle.tsx
@@ -8,7 +8,7 @@ type Props = {
 const SessionTitle = ({ title }: Props) => {
   return (
     <div className='session-title'>
-      <h1 className='bg-layer'>{title}</h1>
+      <h1 className='bg-layer d-none d-lg-block'>{title}</h1>
       <h4 className='title-layer'>{title}</h4>
     </div>
   );
