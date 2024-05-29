c89785e40376b1ea37d6b0f7f3891ed86437c3f2
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-05-29 15:57:23 +0700

    improve the SEO

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 
 import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 
+import { useAppSelector } from '@/store/hooks';
+import { getCurrentCompany } from '@/store/publicCms';
 interface SeoProps {
   title: string;
   description: string;
 }
 
 export const SEO = ({ title, description }: SeoProps) => {
+  const {t} = useTranslation('common');
+  const company = useAppSelector(getCurrentCompany());
+
   return (
     <Helmet>
       {/* Standard metadata tags */}
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      <title>{title || company?.name || t('NctsTitle')}</title>
+      <meta name="description" content={description || company?.name || t('NctsTitle')} />
       {/* End standard metadata tags */}
     </Helmet>
   );
