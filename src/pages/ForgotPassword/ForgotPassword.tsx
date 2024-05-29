ce38ee709ef0550e2eadd42d46752cf173a8682f
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-05-29 15:39:16 +0700

    SEO

--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -2,8 +2,13 @@ import { Button, Form, Input, Space } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
+import { SEO } from '@/components';
+import { useAppSelector } from '@/store/hooks';
+import { getCurrentCompany } from '@/store/publicCms';
+
 export const ForgotPassword = () => {
   const { t } = useTranslation();
+  const company = useAppSelector(getCurrentCompany());
 
   const retrievePassword = (values: any) => {
     console.log(values);
@@ -11,12 +16,16 @@ export const ForgotPassword = () => {
 
   return (
     <div className="d-flex justify-content-center bg-wave p-3 p-lg-5">
+      <SEO
+        title={t('Forgot password', { ns: 'common' }).replace('?', '') + ' - NCTS'}
+        description={company?.name || t('NctsTitle', { ns: 'common' })}
+      />
       <div className="clearfix">
         <p className="h6 text-orange">
           {t('Please enter your email to retrieve your password', { ns: 'common' })}
         </p>
         <p>{t('forgotPasswordDescription', { ns: 'common' })}</p>
-        <Form layout="vertical" className="col-12 col-md-7 col-lg-6">
+        <Form layout="vertical" onFinish={retrievePassword} className="col-12 col-md-7 col-lg-6">
           <Form.Item
             label={t('Email address', { ns: 'common' })}
             name="email"
@@ -24,7 +33,7 @@ export const ForgotPassword = () => {
             rules={[{ required: true }]}>
             <Input placeholder="example@domain.vn" />
           </Form.Item>
-          <Form.Item className='d-flex justify-content-end'>
+          <Form.Item className="d-flex justify-content-end">
             <Space>
               <Link to="/dang-nhap">
                 <Button>{t('Sign In', { ns: 'common' })}</Button>
