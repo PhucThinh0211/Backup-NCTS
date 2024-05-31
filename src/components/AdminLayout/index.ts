8884f180437f61510b632f19f36ee4060f85bd13
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-05-31 10:19:22 +0700

    revise the routes

--- src/components/AdminLayout/index.ts
+++ src/components/AdminLayout/index.ts
@@ -1,3 +1,4 @@
 export { AdminLayout } from './AdminLayout';
 export { AdminHeader } from './AdminHeader';
 export { LeftPanel } from './LeftPanel';
+export { AdminLayout as default } from './AdminLayout';
