5c09f4b57db18566f7ee9b5d2f9c6524b8569227
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-06-19 11:09:16 +0700

    update persist config

--- src/store/reconfigurePersistor.ts
+++ src/store/reconfigurePersistor.ts
@@ -3,10 +3,10 @@ import { createPersistConfig } from './configureStore';
 import { store, persistor } from './';
 import rootReducers from './reducers';
 
-export const reconfigurePersistor = (whitelist: string[]) => {
+export const reconfigurePersistor = async (whitelist: string[]) => {
   localStorage.setItem('persistWhitelist', JSON.stringify(whitelist));
-  const newPersistConfig = createPersistConfig(whitelist);
-  const newPersistedReducer = persistReducer(newPersistConfig, rootReducers);
-  store.replaceReducer(newPersistedReducer);
-  persistor.persist();
+  const newPersistConfig = await createPersistConfig(whitelist);
+  const newPersistedReducer = await persistReducer(newPersistConfig, rootReducers);
+  await store.replaceReducer(newPersistedReducer);
+  await persistor.persist();
 };
