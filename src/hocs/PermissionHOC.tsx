db4451d3e4a035a07f7601c7ec3bf585cab44140
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-06-20 16:06:34 +0700

    permission hoc component

--- src/hocs/PermissionHOC.tsx
+++ src/hocs/PermissionHOC.tsx
@@ -2,14 +2,15 @@ import React, { forwardRef, Ref, ComponentType } from 'react';
 
 import { usePermission } from '@/hooks/usePermission';
 
-interface WithPermissionProps {
+type WithPermissionProps = {
   policyKey?: string;
 }
 
 export function withPermission<P>(Component: ComponentType<P>, policyKey?: string) {
-  const Forwarded = forwardRef<P & WithPermissionProps, Ref<any>>((props: any, ref) => {
-    const isGranted = policyKey || props.policyKey ? usePermission(policyKey || props.policyKey) : true;
-    return isGranted ? <Component ref={ref} {...(props as P)} /> : null;
+  const Forwarded = forwardRef<Ref<any>, P & WithPermissionProps>((props, ref) => {
+    const { policyKey: policyKeyProp, ...rest } = props;
+    const isGranted = policyKey || policyKeyProp ? usePermission(policyKey || policyKeyProp || '') : true;
+    return isGranted ? <Component ref={ref} {...(rest as P)} /> : null;
   });
 
   return Forwarded;
