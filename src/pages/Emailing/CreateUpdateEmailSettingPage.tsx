fee15b3782a8d786769b902cae30882ab1e8726c
HIEU PHAM <hieu_pv@ehicas.com.vn>
2024-06-26 12:14:25 +0700

    send test email

--- src/pages/Emailing/CreateUpdateEmailSettingPage.tsx
+++ src/pages/Emailing/CreateUpdateEmailSettingPage.tsx
@@ -1,10 +1,10 @@
-import {
-  GettingEmailSettingLoadingKey,
-  SavingEmailSettingLoadingKey,
-} from '@/common';
+import { GettingEmailSettingLoadingKey, SavingEmailSettingLoadingKey } from '@/common';
+import { getEnvVars } from '@/enviroment';
 import { adminSettingActions, getEmailSetting } from '@/store/adminSetting';
+import { getAuthenticated } from '@/store/app';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { getLoading } from '@/store/loading';
+import Utils from '@/utils';
 import {
   Typography,
   Button,
@@ -15,14 +15,18 @@ import {
   InputNumber,
   Row,
   Col,
+  Space,
 } from 'antd';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+const { apiUrl } = getEnvVars();
 
 export const CreateUpdateEmailSettingPage = () => {
   const dispatch = useAppDispatch();
   const [form] = Form.useForm();
   const { t } = useTranslation(['common', 'adminSetting']);
+  const auth = useAppSelector(getAuthenticated());
 
   const isSubmmiting = useAppSelector(
     getLoading([SavingEmailSettingLoadingKey, GettingEmailSettingLoadingKey])
@@ -51,32 +55,62 @@ export const CreateUpdateEmailSettingPage = () => {
     dispatch(adminSettingActions.createEmailSettingRequest({ input: values }));
   };
 
+  const [sending, setSending] = useState(false);
+  const sendTestEmail = async () => {
+    setSending(true);
+    const dataTest = {
+      senderEmailAddress: emailSetting?.defaultFromAddress || 'vanhieu@es-vn.com',
+      targetEmailAddress: 'hieupv0809@gmail.com',
+      subject: 'Email subject testing',
+      body: 'This is body....',
+    };
+    try {
+      const response = await fetch(`${apiUrl}/api/setting-management/emailing/send-test-email`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${auth?.token}`,
+        },
+        body: JSON.stringify(dataTest),
+      });
+      setSending(false);
+      if (response.ok) {
+        Utils.successNotification();
+      } else {
+        Utils.errorHandling(await response.json(), 'common');
+      }
+    } catch (error) {
+      setSending(false);
+      Utils.errorHandling(error);
+    }
+  };
+
   return (
-    <div className='p-4'>
-      <div className='d-flex flex-row justify-content-between align-items-center mb-2'>
+    <div className="p-4">
+      <div className="d-flex flex-row justify-content-between align-items-center mb-2">
         <div>
-          <Typography.Title level={4}>
-            {t('Emailing', { ns: 'leftPanel' })}
-          </Typography.Title>
+          <Typography.Title level={4}>{t('Emailing', { ns: 'leftPanel' })}</Typography.Title>
         </div>
-        <div>
-          <Button type='primary' loading={isSubmmiting} onClick={form.submit}>
+        <Space>
+          <Button onClick={sendTestEmail} loading={sending}>
+            Send test email
+          </Button>
+          <Button type="primary" loading={isSubmmiting} onClick={form.submit}>
             {t('OkText', { ns: 'common' })}
           </Button>
-        </div>
+        </Space>
       </div>
       <Form
         form={form}
-        layout='vertical'
+        layout="vertical"
         onFinish={handleSaveEmailSetting}
-        autoComplete='off'
-        autoCorrect='off'
-      >
+        autoComplete="off"
+        autoCorrect="off">
         <Spin spinning={isLoading}>
-          <div className='w-full border-b rounded-2 bg-white p-3 shadow-sm'>
+          <div className="w-full border-b rounded-2 bg-white p-3 shadow-sm">
             <Form.Item
               label={t('Default from display name', { ns: 'adminSetting' })}
-              name='defaultFromDisplayName'
+              name="defaultFromDisplayName"
               rules={[
                 {
                   required: true,
@@ -93,13 +127,12 @@ export const CreateUpdateEmailSettingPage = () => {
                     range2: 1024,
                   }),
                 },
-              ]}
-            >
+              ]}>
               <Input />
             </Form.Item>
             <Form.Item
               label={t('Default from address', { ns: 'adminSetting' })}
-              name='defaultFromAddress'
+              name="defaultFromAddress"
               rules={[
                 {
                   required: true,
@@ -116,15 +149,14 @@ export const CreateUpdateEmailSettingPage = () => {
                     range2: 1024,
                   }),
                 },
-              ]}
-            >
+              ]}>
               <Input />
             </Form.Item>
             <Row gutter={[10, 10]}>
               <Col span={24} md={16}>
                 <Form.Item
                   label={t('Host', { ns: 'adminSetting' })}
-                  name='smtpHost'
+                  name="smtpHost"
                   rules={[
                     {
                       max: 256,
@@ -134,22 +166,13 @@ export const CreateUpdateEmailSettingPage = () => {
                         range2: 256,
                       }),
                     },
-                  ]}
-                >
+                  ]}>
                   <Input />
                 </Form.Item>
               </Col>
               <Col span={24} md={8}>
-                <Form.Item
-                  label={t('Port', { ns: 'adminSetting' })}
-                  name='smtpPort'
-                >
-                  <InputNumber
-                    controls={false}
-                    min={1}
-                    max={65535}
-                    className='w-100'
-                  />
+                <Form.Item label={t('Port', { ns: 'adminSetting' })} name="smtpPort">
+                  <InputNumber controls={false} min={1} max={65535} className="w-100" />
                 </Form.Item>
               </Col>
             </Row>
@@ -157,7 +180,7 @@ export const CreateUpdateEmailSettingPage = () => {
               <Col span={24} md={12}>
                 <Form.Item
                   label={t('Username', { ns: 'adminSetting' })}
-                  name='smtpUserName'
+                  name="smtpUserName"
                   rules={[
                     {
                       max: 1024,
@@ -167,15 +190,14 @@ export const CreateUpdateEmailSettingPage = () => {
                         range2: 1024,
                       }),
                     },
-                  ]}
-                >
+                  ]}>
                   <Input />
                 </Form.Item>
               </Col>
               <Col span={24} md={12}>
                 <Form.Item
                   label={t('Password', { ns: 'adminSetting' })}
-                  name='smtpPassword'
+                  name="smtpPassword"
                   rules={[
                     {
                       max: 1024,
@@ -185,15 +207,14 @@ export const CreateUpdateEmailSettingPage = () => {
                         range2: 1024,
                       }),
                     },
-                  ]}
-                >
+                  ]}>
                   <Input.Password />
                 </Form.Item>
               </Col>
             </Row>
             <Form.Item
               label={t('Domain', { ns: 'adminSetting' })}
-              name='smtpDomain'
+              name="smtpDomain"
               rules={[
                 {
                   max: 1024,
@@ -203,21 +224,15 @@ export const CreateUpdateEmailSettingPage = () => {
                     range2: 1024,
                   }),
                 },
-              ]}
-            >
+              ]}>
               <Input />
             </Form.Item>
 
-            <Form.Item name={'smtpEnableSsl'} valuePropName='checked'>
+            <Form.Item name={'smtpEnableSsl'} valuePropName="checked">
               <Checkbox>{t('Enable ssl', { ns: 'adminSetting' })}</Checkbox>
             </Form.Item>
-            <Form.Item
-              name={'smtpUseDefaultCredentials'}
-              valuePropName='checked'
-            >
-              <Checkbox>
-                {t('Use default credentials', { ns: 'adminSetting' })}
-              </Checkbox>
+            <Form.Item name={'smtpUseDefaultCredentials'} valuePropName="checked">
+              <Checkbox>{t('Use default credentials', { ns: 'adminSetting' })}</Checkbox>
             </Form.Item>
           </div>
         </Spin>
